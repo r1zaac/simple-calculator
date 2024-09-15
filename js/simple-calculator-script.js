4844e42035f8e38e671aae3d734c5b873bb9cf46
r1zaac <muhammad.raza110@gmail.com>
2024-09-15 11:28:35 +0300

    refactor: code formatted

--- js/simple-calculator-script.js
+++ js/simple-calculator-script.js
@@ -3,11 +3,9 @@ let calculation = localStorage.getItem('calculation') || '';
 
 document.querySelector('.js-show').value = calculation
 
-console.log(document.querySelector('.js-show').value = calculation)
 
 function updateCalculation(value) {
   calculation += value;
   document.querySelector('.js-show').value = calculation
-  console.log(calculation);
   localStorage.setItem('calculation', calculation);
 }
